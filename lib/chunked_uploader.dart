61211c7e3ba94045d46b4cc4532e23ed07549d23
Amir Panahandeh <amirpanahandeh@yahoo.com>
2023-01-29 12:06:37 +0330

    Add support for Web
    It's not possible to access file path on Web platform. This commit provides ability to upload file using it's data stream.

--- lib/chunked_uploader.dart
+++ lib/chunked_uploader.dart
@@ -2,6 +2,7 @@ library chunked_uploader;
 
 import 'dart:async';
 import 'dart:math';
+import 'package:async/async.dart';
 import 'package:dio/dio.dart';
 import 'package:path/path.dart' as p;
 import 'package:universal_io/io.dart';
@@ -9,9 +10,35 @@ import 'package:universal_io/io.dart';
 class ChunkedUploader {
   final Dio _dio;
 
-  ChunkedUploader(this._dio);
+  const ChunkedUploader(this._dio);
 
   Future<Response?> upload({
+    required Stream<List<int>> fileDataStream,
+    required String fileName,
+    required int fileSize,
+    required String path,
+    Map<String, dynamic>? data,
+    CancelToken? cancelToken,
+    int? maxChunkSize,
+    Function(double)? onUploadProgress,
+    String method = 'POST',
+    String fileKey = 'file',
+  }) =>
+      UploadRequest(
+        _dio,
+        fileDataStream: fileDataStream,
+        fileName: fileName,
+        fileSize: fileSize,
+        path: path,
+        fileKey: fileKey,
+        method: method,
+        data: data,
+        cancelToken: cancelToken,
+        maxChunkSize: maxChunkSize,
+        onUploadProgress: onUploadProgress,
+      ).upload();
+
+  Future<Response?> uploadWithFilePath({
     required String filePath,
     required String path,
     Map<String, dynamic>? data,
@@ -21,41 +48,61 @@ class ChunkedUploader {
     String method = 'POST',
     String fileKey = 'file',
   }) =>
-      UploadRequest(_dio,
-              filePath: filePath,
-              path: path,
-              fileKey: fileKey,
-              method: method,
-              data: data,
-              cancelToken: cancelToken,
-              maxChunkSize: maxChunkSize,
-              onUploadProgress: onUploadProgress)
-          .upload();
+      UploadRequest.fromFilePath(
+        _dio,
+        filePath: filePath,
+        path: path,
+        fileKey: fileKey,
+        method: method,
+        data: data,
+        cancelToken: cancelToken,
+        maxChunkSize: maxChunkSize,
+        onUploadProgress: onUploadProgress,
+      ).upload();
 }
 
 class UploadRequest {
   final Dio dio;
-  final String filePath, fileName, path,fileKey;
-  final String? method ;
+  late final int fileSize;
+  late final ChunkedStreamReader<int> streamReader;
+  final String fileName, path, fileKey;
+  final String? method;
   final Map<String, dynamic>? data;
   final CancelToken? cancelToken;
-  final File _file;
   final Function(double)? onUploadProgress;
-  late int _maxChunkSize,_fileSize;
+  late int _maxChunkSize;
 
-  UploadRequest(this.dio,
-      {required String this.filePath,
-      required this.path,
-      required this.fileKey,
-      this.method,
-      this.data,
-      this.cancelToken,
-      this.onUploadProgress,
-      int? maxChunkSize})
-      : _file = File(filePath),
-        fileName = p.basename(filePath) {
-    _fileSize = _file.lengthSync();
-    _maxChunkSize = min(_fileSize, maxChunkSize ?? _fileSize);
+  UploadRequest(
+    this.dio, {
+    required Stream<List<int>> fileDataStream,
+    required this.fileName,
+    required this.fileSize,
+    required this.path,
+    required this.fileKey,
+    this.method,
+    this.data,
+    this.cancelToken,
+    this.onUploadProgress,
+    int? maxChunkSize,
+  }) : streamReader = ChunkedStreamReader(fileDataStream) {
+    _maxChunkSize = min(fileSize, maxChunkSize ?? fileSize);
+  }
+
+  UploadRequest.fromFilePath(
+    this.dio, {
+    required String filePath,
+    required this.path,
+    required this.fileKey,
+    this.method,
+    this.data,
+    this.cancelToken,
+    this.onUploadProgress,
+    int? maxChunkSize,
+  }) : fileName = p.basename(filePath) {
+    final file = File(filePath);
+    streamReader = ChunkedStreamReader(file.openRead());
+    fileSize = file.lengthSync();
+    _maxChunkSize = min(fileSize, maxChunkSize ?? fileSize);
   }
 
   Future<Response?> upload() async {
@@ -63,7 +110,7 @@ class UploadRequest {
     for (int i = 0; i < _chunksCount; i++) {
       final start = _getChunkStart(i);
       final end = _getChunkEnd(i);
-      final chunkStream = _getChunkStream(start, end);
+      final chunkStream = _getChunkStream();
       final formData = FormData.fromMap({
         fileKey: MultipartFile(chunkStream, end - start, filename: fileName),
         if (data != null) ...data!
@@ -82,13 +129,12 @@ class UploadRequest {
     return finalResponse;
   }
 
-  Stream<List<int>> _getChunkStream(int start, int end) =>
-      _file.openRead(start, end);
+  Stream<List<int>> _getChunkStream() => streamReader.readStream(_maxChunkSize);
 
   // Updating total upload progress
-  _updateProgress(int chunkIndex, int chunkCurrent, int chunkTotal) {
+  void _updateProgress(int chunkIndex, int chunkCurrent, int chunkTotal) {
     int totalUploadedSize = (chunkIndex * _maxChunkSize) + chunkCurrent;
-    double totalUploadProgress = totalUploadedSize / _fileSize;
+    double totalUploadProgress = totalUploadedSize / fileSize;
     this.onUploadProgress?.call(totalUploadProgress);
   }
 
@@ -97,13 +143,13 @@ class UploadRequest {
 
   // Returning end byte offset of current chunk
   int _getChunkEnd(int chunkIndex) =>
-      min((chunkIndex + 1) * _maxChunkSize, _fileSize);
+      min((chunkIndex + 1) * _maxChunkSize, fileSize);
 
   // Returning a header map object containing Content-Range
   // https://tools.ietf.org/html/rfc7233#section-2
   Map<String, dynamic> _getHeaders(int start, int end) =>
-      {'Content-Range': 'bytes $start-${end - 1}/$_fileSize'};
+      {'Content-Range': 'bytes $start-${end - 1}/$fileSize'};
 
   // Returning chunks count based on file size and maximum chunk size
-  int get _chunksCount => (_fileSize / _maxChunkSize).ceil();
+  int get _chunksCount => (fileSize / _maxChunkSize).ceil();
 }
